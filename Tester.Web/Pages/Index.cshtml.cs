a49bf4de94678d0f6ba1fe147c774bcfc8653e9b
hamidreza hematyar <hamidrez77@gmail.com>
2020-08-11 00:09:58 +0430

    create aspcore webapp example

---
+++ Tester.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,55 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Dtx.Net.Mail;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Tester.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IEmailSender _emailSender;
+
+        public IndexModel(ILogger<IndexModel> logger,
+            IEmailSender emailSender)
+        {
+            _logger = logger;
+            _emailSender = emailSender;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [DataType(DataType.MultilineText)]
+            public string Message { get; set; }
+
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var mailAddress = new MailAddress(Input.Email);
+                await _emailSender.Send(mailAddress, "sample", Input.Message, MailPriority.Normal);
+            }
+            return Page();
+        }
+    }
+}
